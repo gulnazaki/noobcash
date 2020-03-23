7545682e7cd2ae8913cde06df9c82a866d65a910
phoebkal <phoekalogiannis@gmail.com>
2020-03-23 19:42:53 +0200

    cli main and cli functions

---
+++ cli.py
@@ -0,0 +1,33 @@
+import argparse
+import sys
+from cli_functions import *
+""" 
+Command line inteface
+t <recipient_address> <amount> : sends a certain amount of noobcash to the given address 
+view						   : show last verified block of transactions
+balance     				   : show the balance of the user's wallet
+help
+"""
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(prog='cli.py', description='NoobCash Wallet CLI:Command Line inteface for the owner of a NoobCash wallet.')
+	subparsers = parser.add_subparsers(dest='scope')
+	# transaction
+	t_parser = subparsers.add_parser('t', description='t : Send NoobCash. New transaction between current user and a given IP address.')
+	t_parser.add_argument('recip_addr', nargs=1, help='The IP address of the recipient in the new transaction.')
+	t_parser.add_argument('amount', nargs=1, help='The amount of NoobCash you wish to pay to the given address.')
+	t_parser.set_defaults(func=transaction)
+	# view
+	v_parser = subparsers.add_parser('view', description='View  : View last transactions in the last validated block.')
+	v_parser.set_defaults(func=view)
+	# balance
+	b_parser = subparsers.add_parser('balance', description='Balance : View your balance, the amount of NoobCash in your wallet.')
+	b_parser.set_defaults(func=balance)
+	#
+	# Parse
+	args = parser.parse_args()
+
+	if not len(sys.argv) > 1:
+		parser.print_help(sys.stderr)
+		sys.exit(1)
+
+	args.func(args)
---
+++ cli_functions.py
@@ -0,0 +1,13 @@
+def transaction(args):
+	sender_wallet()
+	
+	# print(args.recip_addr, args.amount)
+	return 0
+
+def view(args):
+	# print('view')
+	return 0
+
+def balance(args):
+	# print('balance')
+	return 0
