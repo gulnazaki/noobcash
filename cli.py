f493fc5be443f02f01f06ec35cbfdb7e02b9834d
phoebkal <phoekalogiannis@gmail.com>
2020-04-03 21:32:45 +0300

    debugging

--- cli.py
+++ cli.py
@@ -9,13 +9,13 @@ balance     				   : show the balance of the user's wallet
 help
 """
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(prog='cli.py', description='NoobCash Wallet CLI:Command Line inteface for the owner of a NoobCash wallet.')
+	parser = argparse.ArgumentParser(prog='cli.py', conflict_handler='resolve', description='NoobCash Wallet CLI:Command Line inteface for the owner of a NoobCash wallet.')
 	subparsers = parser.add_subparsers(dest='scope')
 	# transaction
 	t_parser = subparsers.add_parser('t', description='t : Send NoobCash. New transaction between current user and a given IP address.')
-	t_parser.add_argument('sender_addr', nargs=1, help='The IP address of the sender in the new transaction.')
-	t_parser.add_argument('recip_addr', nargs=1, help='The IP address of the recipient in the new transaction.')
-	t_parser.add_argument('amount', nargs=1, help='The amount of NoobCash you wish to pay to the given address.')
+	t_parser.add_argument('-sender_addr', nargs=1, help='The IP address of the sender in the new transaction.')
+	t_parser.add_argument('-recip_addr', nargs=1, help='The IP address of the recipient in the new transaction.')
+	t_parser.add_argument('-amount', nargs=1, help='The amount of NoobCash you wish to pay to the given address.')
 	t_parser.set_defaults(func=transaction)
 	# view
 	v_parser = subparsers.add_parser('view', description='View  : View last transactions in the last validated block.')
